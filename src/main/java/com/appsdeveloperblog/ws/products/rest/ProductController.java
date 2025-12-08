12b7bedd034161f6a868f4afc4ef8c12c85d31e5
Francisco Guiraldelli <francisco.guiraldelli@gmail.com>
2025-12-08 21:49:24 +0100

    Using Lombok to create a logger instead creating variable LOGGER in Controller class

--- src/main/java/com/appsdeveloperblog/ws/products/rest/ProductController.java
+++ src/main/java/com/appsdeveloperblog/ws/products/rest/ProductController.java
@@ -1,43 +1,33 @@
 package com.appsdeveloperblog.ws.products.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import com.appsdeveloperblog.ws.products.service.ProductService;
-import java.util.Date;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
+import com.appsdeveloperblog.ws.products.service.ProductService;
+import lombok.extern.slf4j.Slf4j;
+import java.util.Date;
 
-
+@Slf4j
 @RestController
 @RequestMapping("/products")
 public class ProductController {
 
-  ProductService productService;
-  private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+  private final ProductService productService;
 
   public ProductController(ProductService productService) {
     this.productService = productService;
   }
 
   @PostMapping
-  public ResponseEntity<Object> createProduct(@RequestBody CreatedProductRestModel product){
-
-    String productId;
+  public ResponseEntity<Object> createProduct(@RequestBody CreatedProductRestModel product) {
     try {
-      productId = productService.createProduct(product);
+      String productId = productService.createProduct(product);
+      return ResponseEntity.status(HttpStatus.CREATED).body(productId);
+
     } catch (Exception e) {
-      // e.printStackTrace();
-      LOGGER.error(e.getMessage(), e);
-      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-      .body(new ErrorMessage(new Date(), e.getMessage(), "/products"));
+      log.error(e.getMessage(), e);
+      ErrorMessage errorMessage = new ErrorMessage(new Date(), e.getMessage(), "/products");
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
     }
-
-    return ResponseEntity.status(HttpStatus.CREATED).body(productId);
   }
-
-
 }
